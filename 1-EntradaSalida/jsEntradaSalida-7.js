7ae210f46304c2ca9391994563f2ae26f739ef2b
Hellding <arubar15@outlook.es>
2017-02-02 11:21:14 -0300

    Ejericio 7 Ok

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,53 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var uno;
+	var dos;
+	var conclusion;
+	uno=document.getElementById("numeroUno").value;
+	dos=document.getElementById("numeroDos").value;
+	uno=parseInt(uno);
+	dos=parseInt(dos);
+	conclusion=uno+dos;
+	alert("la suma es "+conclusion);
 }
 
 function restar()
 {
-	
+	var uno;
+	var dos;
+	var conclusion;
+	uno=document.getElementById("numeroUno").value;
+	dos=document.getElementById("numeroDos").value;
+	uno=parseInt(uno)
+	dos=parseInt(dos)
+	conclusion=uno-dos
+	alert("la resta es "+conclusion)
 }
 
 function multiplicar()
 { 
-	
+	var uno;
+	var dos;
+	var conclusion;
+	uno=document.getElementById("numeroUno").value;
+	dos=document.getElementById("numeroDos").value;
+	uno=parseInt(uno)
+	dos=parseInt(dos)
+	conclusion=uno*dos
+	alert("la multiplicacion es "+conclusion)
 }
 
 function dividir()
 {
-	
+	var uno;
+	var dos;
+	var conclusion;
+	uno=document.getElementById("numeroUno").value;
+	dos=document.getElementById("numeroDos").value;
+	uno=parseInt(uno)
+	dos=parseInt(dos)
+	conclusion=uno/dos
+	alert("la division es "+conclusion)
 }
 
