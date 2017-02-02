9ebdcc187b567fe34fdfc6c97faeae17c0a8b0cb
Hellding <arubar15@outlook.es>
2017-02-02 09:50:45 -0300

    go4

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+	var nombre= " agustin "
+	var edad=" 18"
+	document.getElementById("elNombre").value= " agustin ";
+	document.getElementById("laEdad").value=" 18";
+var msj= "hola me llamo" + nombre + "y mi edad es" + edad
+	prompt ("agustin","18")
 }
 
