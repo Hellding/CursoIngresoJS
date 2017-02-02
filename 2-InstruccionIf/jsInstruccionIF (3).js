78f8dcacfc8d3b06ea28580a840a9b327686b4f3
Hellding <arubar15@outlook.es>
2017-02-02 12:14:24 -0300

    Variable 3 OK

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,10 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad=document.getElementById("edad").value;
+edad=parseInt(edad)
+if(edad>18){alert("sos mayor de edad")}
+if(edad<18){alert("sos menor de edad")}
 
 }//FIN DE LA FUNCIÓN
