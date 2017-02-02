593ce367a64335aebaf15a541d2cfc994b93191f
Hellding <arubar15@outlook.es>
2017-02-02 11:36:16 -0300

    Ejercicio 9 Ok

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,12 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-
-	
+	var uno;
+	var dos;
+	var resultado;
+	uno=document.getElementById("sueldo").value;
+	dos=1.10;
+	uno=parseInt(uno);
+	resultado=uno*dos;
+	alert("el aumento es de "+resultado)
 }
