b8d9745d72a895af922dda1ff112cc0133f27b83
Hellding <arubar15@outlook.es>
2017-02-02 12:25:56 -0300

    Variable 4 Incompleto

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,10 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad=document.getElementById("edad").value;
+edad=parseInt(edad)
+if(edad<13>17){alert(sos adolescente)}
 
 
 }//FIN DE LA FUNCIÓN
