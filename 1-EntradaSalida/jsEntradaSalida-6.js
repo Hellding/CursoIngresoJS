e12a43b52cdb04fa0abb3ac0f93ebe84f48d8a49
Hellding <arubar15@outlook.es>
2017-02-02 10:44:05 -0300

    Signed-off-by: Hellding <arubar15@outlook.es>

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,13 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	var uno;
+	var dos;
+	var conclusion;
+	a=document.getElementById("numeroUno").value;
+	b=document.getElementById("numeroDos").value;
+	uno=parseInt(a);
+	dos=parseInt(b);
+	conclusion="la suma es"+ uno+dos ;
+	alert(conclusion);
 }
-
