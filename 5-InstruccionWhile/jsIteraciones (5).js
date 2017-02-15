36651a07e55e8fb9847592d3c9326a493aca7031
Hellding <arubar15@outlook.es>
2017-02-15 12:22:02 -0300

    W4,W5,W6 OK

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,9 +2,9 @@ function Mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
-
-
-
 document.getElementById('Sexo').value=sexo;
-
+while(sexo!="f"&&sexo!="m")
+	{
+		sexo = prompt("ingrese f ó m .")	
+	}
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,11 +1,17 @@
 function Mostrar()
 {
-
-	var contador=0;
+	var numer=0
+	var contador;
 	var acumulador=0;
-
-
-
+while(numer<5)
+	{
+		numer++
+		contador=prompt("");
+		contador=parseInt(contador);
+		acumulador+=contador;
+	}
+	alert(acumulador);
+	alert(acumulador/contador)
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
 
