5533f3f85e9d103003be643a15874b469a8240dd
Hellding <arubar15@outlook.es>
2017-02-16 11:06:29 -0300

    W8 OK

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,13 +1,29 @@
 function Mostrar()
 {
 
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
-	
-	var respuesta='si';
-
-
+var contador=0;
+var numero;
+var positivo=0;
+var negativo=1;	
+var respuesta='si';
+respuesta=confirm("quieres comenzar?");
+while(respuesta)
+	{
+	numero=prompt("");
+	numero=parseInt(numero);
+	contador++;
+		if(numero>=0)
+		{
+			positivo+=numero;
+		}
+		else(numero<0)
+		{
+			negativo*=numero;
+		}
+	respuesta=confirm("quieres continuar?")
+	}
+	alert("la suma de los positivos es "+positivo)
+	alert("el producto de los negativos es "+negativo*contador)
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
 
