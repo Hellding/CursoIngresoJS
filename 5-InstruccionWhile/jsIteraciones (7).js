54d9d16691728b826a019ef6aefb4e2716890c9a
alumno <alumno>
2017-02-16 10:04:03 -0300

    W7 Ok

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,20 @@
 function Mostrar()
 {
-
 	var contador=0;
+	var numero;
 	var acumulador=0;
-	var respuesta='si';
-
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
-
+	var respuesta="si";
+	respuesta=confirm("quieres comenzar?");
+	while(respuesta)
+	{
+		numero=prompt("");
+		numero=parseInt(numero);
+		acumulador+=numero;
+		contador++;
+		respuesta=confirm("quieres continuar?");
+	}
+	alert(acumulador);
+	alert(acumulador/contador);
+document.getElementById("suma").value=acumulador;
+document.getElementById("promedio").value=acumulador/contador;
 }//FIN DE LA FUNCIÓN
