1da9bd6bdabb6937d2d73c612c54598b623be664
Hellding <arubar15@outlook.es>
2017-02-16 12:21:17 -0300

    W9 OK, W10 Completar

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,51 @@
 function Mostrar()
 {
-
+	var numero;
 	var contador=0;
 	//declarar contadores y variables 
-	
+	var positivos;
+	var negativos;
+	var cantpos=0;
+	var cantneg=0;
+	var cantdeo=0;
+	var cantdenpares=0;
+	var promdepos;
+	var promdeneg;
 	var respuesta="si";
-
+	respuesta=prompt("comenzar el programa?")
 	while(respuesta!="no")
 	{
-		
-	
+		numero=prompt("");
+		numero=parseInt(numero);
+		if(numero>0)
+		{
+			cantpos++;
+			positivos+=numero;
+			promdepos/=numero;
+		}
+		if(numero<0)
+		{
+			cantneg++;
+			negativos+=numero;
+			promdeneg/=numero;
+		}
+		if(numero=0)
+		{
+			cantdeo++;
+		}
+		if(numero%2==0)
+		{
+			cantdenpares++;
+		}
+	respuesta=confirm("quieres continuar?")
 	}
-
-
-
-
+	alert("la suma de los negativos es "+negativos)
+	alert("la suma de los positivos es "+positivos)
+	alert("la cantidad de positivos es de "+cantpos)
+	alert("la cantidad de negativos es de "+cantneg)
+	alert("la cantidad de 0 es de "+cantdeo)
+	alert("la cantidad de numeros pares es de "+cantdenpares)
+	alert("el promedio de numeros positivos es de "+promdepos)
+	alert("el promedio de numeros negativos es de "+promdeneg)
+	alert("la diferancia entre posistivos y negativos es de "+posistivos-negativos)
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,35 @@ function Mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
-
-	while(respuesta!='no')
+	var numero;
+	var maximo;
+	var minimo;
+	var respuesta;
+	var primera=true;
+	respuesta=confirm("Quiere comenzar?")
+	while(respuesta)
 	{
-		
-	
+		numero=prompt("");
+		numero=parseInt(numero);
+		if(primera)
+		{
+			maximo=numero;
+			minimo=numero;
+			primera=false;
+		}
+		else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+			if(numero<minimo)
+			{
+				minimo=numero
+			}
+		}
+		respuesta=confirm("quieres continuar?");
 	}
-
-
-
-
+	alert("el numero maximo es "+maximo)
+	alert("el munero minimo es "+minimo)
 }//FIN DE LA FUNCIÓN
